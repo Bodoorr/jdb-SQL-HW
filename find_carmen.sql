adced5c0b1986152551ab065faf618594f21f453
Bodoorr <bodoor5@hotmail.com>
2026-09-20 14:10:09 +0300

    Clue 2 solved

--- find_carmen.sql
+++ find_carmen.sql
@@ -5,17 +5,21 @@
 SELECT * from country WHERE region='Southern Europe' order by population ASC
 -- The answer: Holy See (Vatican City State)
 
-
+--"VAT"	"Holy See (Vatican City State)"	"Europe"	"Southern Europe"	0.4	1929	1000		9.00		"Santa Sede/Cittï¿½ del Vaticano"	"Independent Church State"	"Johannes Paavali II"	3538	"VA"
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+SELECT * from countrylanguage WHERE countrycode='VAT' AND isofficial='true'
+-- The answer : Italian
 
+--"VAT"	"Italian"	true	0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+
 
 
 
